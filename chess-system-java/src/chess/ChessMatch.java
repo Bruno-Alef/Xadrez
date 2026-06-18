5e40f39b9ec1bbd9e41f225119350277c043ca9a
luishenriqueanjos <luishenriquead10@gmail.com>
2026-06-18 20:52:46 -0300

    Anteriormente, foram feitas adições no ChessMatch, não mensionadas (além das renomeções). Foram adicionadas os atributos turn, currentPlayer e board; o construtor inicializa o tabuleiro 8x8 e define o jogador inicial; e o método getPieces() expõe a matriz de peças convertida para o tipo ChessPiece.

--- chess-system-java/src/chess/ChessMatch.java
+++ chess-system-java/src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -12,6 +14,7 @@ public class ChessMatch {
 		board = new Board(8, 8);
 		turn = 1;
 		currentPlayer = Color.WHITE;
+		initialSetup();
 	}
 
 	public int getTurn() {
@@ -30,5 +33,19 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+
+	private void initialSetup() {
+		placeNewPiece('h', 7, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
+
+		placeNewPiece('b', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
 	}
 }
